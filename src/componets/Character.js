7a96c2d8c70479eef8cdcc84a86091567c0ab3be
ced277 <re.contreras.a@outlook.com>
2022-09-13 12:57:27 -0500

    primer commi

---
+++ src/componets/Character.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default function Character(props) {
+    
+   const  {characters, setCharacters}= props
+
+   const resetCharacters = () => {
+    setCharacters(null)
+
+   }
+   
+  return (
+    <div className='characters'>
+       <h2>Personajes</h2> 
+       <span className='back-home' onClick={resetCharacters}>Volver a home</span>
+       <div className='container-characters'>
+        {characters.map((character,index)=>(
+            <div className='character-container' key={index}>  
+            <div>
+
+                <img src={character.image} alt={character.name}></img>
+            </div>
+            <div>
+                <h3>{character.name}</h3>
+                <h6>
+                    {character.status === "Alive" ? (
+                        <>
+                        <span className='alive'> Alive</span>
+                        </>
+                    ) : (
+                        <>
+                        <span className='dead'>
+                        Dead
+                        </span>
+                        </>
+                    )}
+                </h6>
+                <p>
+                <span className='text-grey'>Episodio : </span>
+                <span>{character.episode.length}</span>
+                </p>
+                <p>
+                <span className='text-grey'>Especie : </span>
+                <span>{character.species}</span>
+                </p>
+            </div>
+            
+            </div>
+
+        ))}
+
+       </div>
+       <span className='back-home' onClick={resetCharacters}>Volver a home</span>
+    </div>
+  )
+}
